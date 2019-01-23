b6f9d7dc49e1f616299d31d5204f147826613563
dangirasv <46868659+dangirasv@users.noreply.github.com>
2019-01-23 17:35:52 +0200

    Add files via upload
    Changed to results be saved on TXT  file sepparated by tabs

--- id_size.py
+++ id_size.py
@@ -1,7 +1,7 @@
 import csv
 
 filename = 'data.csv'
-results_f = 'results.csv'
+results_f = 'results.txt'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -21,9 +21,17 @@ for i in range(len(header_row)-1):
         clm1.append(row[0])
         clm2.append(header_row[i+1])
         clm3.append(row[i+1])
-
+"""
 with open(results_f, mode='w') as results:
     writer = csv.writer(results, delimiter=',')
     for ind in range(len(clm1)):
         writer.writerow([clm1[ind], clm2[ind], clm3[ind]])
     results.close()
+"""
+results = open(results_f, "w")
+results.write(str(clm1[0]) + "\t" + str(clm2[0]) + "\t" + str(clm3[0]))
+results.close()
+with open(results_f, "a") as results:
+    for ind in range(len(clm1)-1):
+        results.write("\n" + str(clm1[ind+1]) + "\t" + str(clm2[ind+1]) + "\t" + str(clm3[ind+1]))
+results.close()
